3c1f9fe5e04a65184c0f929c8e91d1cd7a0347ca
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-06-21 11:06:01 -0300

    Criando a estrutura para o botCurso

--- projetos/listaCompras/listaComprasGrupos.js
+++ projetos/listaCompras/listaComprasGrupos.js
@@ -8,26 +8,26 @@ let dados = {}
 
 const gerarBotoes = lista => Extra.markup(
     Markup.inlineKeyboard(
-        lista.map(item => Markup.callbackButton(item, `delete ${item}`)),
-        {columns: 3}
+        lista.map(item => Markup.callbackButton(item, `delete ${item}`))
+            , {columns: 3}
     )
 )
 
 bot.start(async ctx => {
-    const name = ctx.update.message.from.first_name 
+    const name = ctx.update.message.from.first_name
     await ctx.reply(`Seja bem vindo, ${name}!`)
     await ctx.reply('Escreva os itens que você deseja adicionar...')
 })
 
 bot.use((ctx, next) => {
-    const chatId = ctx.chat.id 
+    const chatId = ctx.chat.id
     if(!dados.hasOwnProperty(chatId)) dados[chatId] = []
     ctx.itens = dados[chatId]
     next()
 })
 
 bot.on('text', ctx => {
-    let texto = ctx.update.message.text 
+    let texto = ctx.update.message.text
     if(texto.startsWith('/')) texto = texto.substring(1)
     ctx.itens.push(texto)
     ctx.reply(`${texto} adicionado!`, gerarBotoes(ctx.itens))
